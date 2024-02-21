422734f73b1ffd2fa863bb080598ba5d853bbe56
Matheus Jerônimo <74997962+matheusjeronimomo@users.noreply.github.com>
2024-02-20 22:38:56 -0300

    Update remove-audio.py
    Simplifiquei o arquivo

--- remove-audio.py
+++ remove-audio.py
@@ -1,21 +1,6 @@
 import moviepy
 import moviepy.editor
-from tkinter import Tk, Button
 
-def extraiAudio(camVideo,camAudio):
-	video = moviepy.editor.VideoFileClip(camVideo)
-	audio = video.audio
-	audio.write_audiofile(camAudio)
-
-def escolheArquivo():
-	pass
-
-app = Tk()
-app.title("Extrair audio do vídeo")
-app.geometry("400x400")
-
-botaoEscolheArquivo = Button(app, text="Escolha o arquivo", command=escolheArquivo)
-botaoEscolheArquivo.grid(column=0,row=0)
-
-
-app.mainloop()
+video = moviepy.editor.VideoFileClip("Nome do vídeo")
+audio = video.audio
+audio.write_audiofile("Nome do arquivo")
